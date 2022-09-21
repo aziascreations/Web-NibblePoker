97336cb8b5ae1c241d1c4fbcde48de3f5f6fc18f
Herwin <herwin.bozet@gmail.com>
2022-09-21 17:30:32 +0200

    Improved caching, Fixed sitemap Update .htaccess, headers.php, and sitemap.txt

--- commons/headers.php
+++ commons/headers.php
@@ -9,10 +9,9 @@ if(basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
 <meta name="viewport" content="width=device-width" />
-<!-- TODO: Change to proper values once finished ! -->
-<meta content="no-cache, no-store, must-revalidate" http-equiv="Cache-Control">
-<meta content="no-cache" http-equiv="Pragma">
-<meta content="0" http-equiv="Expires">
+<meta content="cache, store" http-equiv="Cache-Control">
+<meta content="cache, store" http-equiv="Pragma">
+<meta content="3600" http-equiv="Expires">
 <meta name="theme-color" content="#1D2023">
 <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 <link rel="alternate icon" href="/favicon.ico">
