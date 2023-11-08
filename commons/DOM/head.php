ebba159b4378500d39ec08c0bde63acb2ec64f5d
Herwin <herwin.bozet@gmail.com>
2023-11-08 01:04:07 +0100

    Cleaned up some code, Replaced robots.txt & sitemap.txt with PHP code, Updated gitignore rules Update .gitignore, .htaccess, and 11 more files...

--- commons/DOM/head.php
+++ commons/DOM/head.php
@@ -9,21 +9,11 @@ if(basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport"/>
 <meta name="viewport" content="width=device-width"/>
-<?php
-// Caching should be handled in '.htaccess' !
-//<!--<meta content="cache, store" http-equiv="Cache-Control">
-//<meta content="cache, store" http-equiv="Pragma">
-//<meta content="3600" http-equiv="Expires">-->
-?>
 <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 <link rel="alternate icon" href="/favicon.ico">
 <link rel="stylesheet" href="/resources/FontAwesomePro/5.15.3/css/all.min.css">
 <link rel="stylesheet" href="/resources/NibblePoker/css/nibblepoker.min.css?v=1">
 <?php
-if($enable_waffle_iron) {
-	// Turns out you can't change an SVG's path fill if it's inside an IMG tag...
-	//echo("<link rel=\"stylesheet\" href=\"/resources/NibblePoker/css/waffle-day.min.css\">");
-}
 if($enable_code_highlight) {
 	echo('<link href="/resources/HighlightJS/11.6.0/styles/atom-one-dark.min.css" rel="stylesheet"/>');
 }
--- commons/config.php
+++ commons/config.php
@@ -4,6 +4,7 @@ if(basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
 	header('HTTP/1.1 403 Forbidden'); die();
 }
 
+// Only being used in 'head' for opengraph data.
 $host = "nibblepoker.lu";
 $host_uri = "https://nibblepoker.lu";
 
